f9e323d8b057b7028bc0a4f44b03ebe82c1e148a
isayme <isaymeorg@gmail.com>
2022-11-18 01:49:49 +0800

    update

--- load.sh
+++ load.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-YYYY=$(date -u +"%Y")
-MM=$(date -u +"%m")
+YYYY=$(date +"%Y")
+MM=$(date +"%m")
 
-YYYYMM=$(date -u +"%Y%m")
-YYYYMMDD=$(date -u +"%Y%m%d")
+YYYYMM=$(date +"%Y%m")
+YYYYMMDD=$(date +"%Y%m%d")
 
-WEFOUND_CC_URL=https://wefound.cc/freenode/${YYYYMM}/${YYYYMMDD}.yaml
 NODEFREE_ORG_URL=https://nodefree.org/dy/${YYYYMM}/${YYYYMMDD}.yaml
 CLASH_NODE_COM_URL=https://clashnode.com/wp-content/uploads/${YYYY}/${MM}/${YYYYMMDD}.yaml
 
-echo downloading $WEFOUND_CC_URL ...
-wget $WEFOUND_CC_URL -O ./public/wefoundcc.yaml
-
 echo downloading $NODEFREE_ORG_URL ...
-wget $NODEFREE_ORG_URL -O ./public/nodefreeorg.yaml
+FILENAME=nodefreeorg.yaml
+(wget $NODEFREE_ORG_URL -O ./public/${FILENAME}.tmp && mv ./public/${FILENAME}.tmp ./public/${FILENAME}) || (rm ./public/${FILENAME}.tmp && echo "remove ${FILENAME}.tmp")
 
 echo downloading $CLASH_NODE_COM_URL ...
-wget $CLASH_NODE_COM_URL -O ./public/clashnodecom.yaml
+FILENAME=clashnodecom.yaml
+(wget $CLASH_NODE_COM_URL -O ./public/${FILENAME}.tmp && mv ./public/${FILENAME}.tmp ./public/${FILENAME}) || (rm ./public/${FILENAME}.tmp && echo "remove ${FILENAME}.tmp")
